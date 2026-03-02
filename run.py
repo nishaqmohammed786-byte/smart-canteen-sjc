99735f7a1d573e5bbf7df897943854c01af86a61
NishaqMohammed <nishaqmohammed786@gmail.com>
2026-03-03 02:27:07 +0530

    run.py updated

--- run.py
+++ run.py
@@ -1,17 +1,13 @@
 from backend.app import app, socketio
-from backend.database.db_config import get_local_ip
-import socket
+import os
 
 if __name__ == "__main__":
-    local_ip = get_local_ip()
-    
+    port = int(os.environ.get("PORT", 5000))
+
     print("\n" + "="*50)
     print("🚀 SJC CANTEEN SERVER STARTING...")
     print("="*50)
-    print(f"📍 Local URL: http://127.0.0.1:5000")
-    print(f"📍 Network URL: http://{local_ip}:5000")
-    print(f"📱 Mobile URL: http://{local_ip}:5000 (same WiFi)")
+    print(f"📍 Running on PORT: {port}")
     print("="*50 + "\n")
-    
-    # Run on all interfaces (0.0.0.0) so both localhost and IP work
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
+    socketio.run(app, host="0.0.0.0", port=port)
